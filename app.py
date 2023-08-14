786fd09f1adf3c2103e86c57704128080b366874
rryanhermes <106575167+rryanhermes@users.noreply.github.com>
2023-08-14 16:04:49 -0500

    added test app route

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__, static_folder='static', template_folder='html')
 def home():
     return render_template('index.html')
 
+@app.route("/test")
+def test():
+    return render_template('test.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
